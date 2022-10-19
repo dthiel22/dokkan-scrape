67b0b4a646380e5507078883f1360337bb6f260b
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-10-18 17:05:22 -0700

    update: attributes stored as objects....see if storage in object works when some inputs are not present....next to do is store in an array....then run multiple

--- extract_source.py
+++ extract_source.py
@@ -12,6 +12,48 @@ import io
 # print(buf.readline())
 
 # UNIVERSAL VAR
+charHolding = []
+class CharAttrs:
+    def __init__(self, name1, name2, rarity, cType, ID, LS, SaType, SaDesc, Ultra, UltraDesc, PSName, PSDesc, ASName, AS, ASCond, TransformType, TransformCond, Links, Categories):
+        self.name1 = name1
+        self.name2 = name2
+        self.rarity = rarity
+        self.cType = cType
+        self.ID = ID
+        self.LS = LS
+        self.SaType = SaType
+        self.SaDesc = SaDesc
+        self.Ultra = Ultra
+        self.UltraDesc = UltraDesc
+        self.PSName = PSName
+        self.PSDesc = PSDesc
+        self.ASName = ASName
+        self.AS = AS
+        self.ASCond = ASCond
+        self.TransformType = TransformType
+        self.TransformCond = TransformCond
+        self.Links = Links
+        self.Categories = Categories
+
+charName1 = ""
+charName2 = ""
+charRarity = ""
+charType = ""
+charID = ""
+charLS = ""
+charSaType = ""
+charSaDesc = ""
+charUltra = ""
+charUltraDesc = ""
+charPsName = ""
+charPsDesc = ""
+charActiveSkillName = ""
+charActiveSkill = ""
+charActiveSkillCond = ""
+charTransformType = ""
+charTransformCond = ""
+charLinkSkills = ""
+charCategories = ""
 
 URL = "https://dbz-dokkanbattle.fandom.com/wiki/Planet-Crushing_Blow_Cooler_(Final_Form)?action=edit"
 page = requests.get(URL)
@@ -48,9 +90,6 @@ while myline:
     if '|name2' in myline:
         print(">>nametwo found<<")
         charName2 = myline
-    if '|Active skill =' in myline:
-        print(">>Active Skill found<<")
-        charActiveSkill = myline
     if '|rarity =' in myline:
         print(">>rarity found<<")
         charRarity = myline
@@ -60,43 +99,75 @@ while myline:
     if '|ID =' in myline:
         print(">>ID found<<")
         charID = myline
-    if '|LS' in myline:
+    if '|LS description =' in myline:
         print(">>LS found<<")
         charLS = myline
-    if 'SA type =' in myline:
+    if '|SA type =' in myline:
         print(">>SA type found<<")
         charSaType = myline
     if '|SA description =' in myline:
-        print(">>SA type found<<")
+        print(">>SA description found<<")
         charSaDesc = myline
     if '|UltraSA name =' in myline:
-        print(">>SA type found<<")
+        print(">>UltraSA found<<")
         charUltra = myline
     if '|UltraSA description =' in myline:
-        print(">>SA type found<<")
+        print(">>UltraSa Description found<<")
         charUltraDesc = myline
     if '|PS name =' in myline:
-        print(">>SA type found<<")
+        print(">>PS name found<<")
         charPsName = myline
     if '|PS description =' in myline:
-        print(">>SA type found<<")
+        print(">>PS descriotion found<<")
         charPsDesc = myline
-    if '|PS description =' in myline:
-        print(">>SA type found<<")
-        charPsDesc = myline
+    if '|Active skill name =' in myline:
+        print(">>Active skill name found<<")
+        charActiveSkillName = myline
+    if '|Active skill =' in myline:
+        print(">>Active skill found<<")
+        charActiveSkill = myline
+    if '|Active skill condition =' in myline:
+        print(">>Active skill condition found<<")
+        charActiveSkillCond = myline
+    if '|Transform type =' in myline:
+        print(">>Transform type found<<")
+        charTransformType = myline
+    if '|Transform condition =' in myline:
+        print(">>Transform description found<<")
+        charTransformCond = myline
+    if '|Link skill =' in myline:
+        print(">>Link skills found<<")
+        charLinkSkills = myline
+    if '|Category =' in myline:
+        print(">>Link skills found<<")
+        charCategories = myline
 
-print('end loop')
+
+print('+++++++++++end loop+++++++++++')
 print('')
-print(charName1)
-print(charName2)
-print(charActiveSkill)
-print(charRarity)
-print(charType)
-print(charID)
-print(charLS)
-print(charSaType)
-print(charSaDesc)
-print(charUltra)
-print(charUltraDesc)
-print(charPsName)
-print(charPsDesc)
+
+c1 = CharAttrs(charName1, charName2, charRarity, charType, charID, charLS, charSaType, charSaDesc, charUltra, charUltraDesc, charPsName, charPsDesc, charActiveSkillName, charActiveSkill, charActiveSkillCond, charTransformType, charTransformCond, charLinkSkills, charCategories)
+
+
+# if charName1 == "":
+#     print('no name found')
+# print(charName2)
+# print(charRarity)
+# print(charType)
+# print(charID)
+# print(charLS)
+# print(charSaType)
+# print(charSaDesc)
+# print(charUltra)
+# print(charUltraDesc)
+# print(charPsName)
+# print(charPsDesc)
+# if charActiveSkillName == "":
+#     print('no active skill found')
+# print(charActiveSkillName)
+# print(charActiveSkill)
+# print(charActiveSkillCond)
+# print(charTransformType)
+# print(charTransformCond)
+# print(charLinkSkills)
+# print(charCategories)
