86ec9f7af40e6f5f90b0fd7a7c8a450dbdcb68e2
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-10-20 19:37:31 -0700

    the basic structure is there and operating....I might try and redo the grabbing of the material and THEN trimming it down

--- AAA_extract_source.py
+++ AAA_extract_source.py
@@ -5,19 +5,18 @@ from turtle import right
 import requests
 from bs4 import BeautifulSoup
 import io
-import json
 
 # loops through pages of cards x < 2600 ========
-x = 1
-while x < 300:
-    x += 100
+x = 2200
+while x < 2300:
+    x += 1
     print()
     cardPage = f"https://dbz-dokkanbattle.fandom.com/wiki/All_Cards:_(1){x}_to_(1){x+99}"
     print(cardPage)
     print("=========")
     i = 0
     # iteration needs to be i <= 99 in order to loop through all of it
-    while i <= 10:
+    while i <= 99:
         i += 1
         URL = cardPage
         page = requests.get(URL)
@@ -39,158 +38,126 @@ while x < 300:
         main = BeautifulSoup(page.content, "html.parser")
         content = main.find(id="mw-content-text")
         textBoxEl = content.find(id='wpTextbox1').text
+        # this will print the PRE-EDIT RESULTS
         # print(results)
 
-        # adding top line for formatting (doesn't cut character name when iterating later) and = => + for formating =====================
+        # all page formatting =====================
         print('')
         addLineToTop = textBoxEl.replace("{{", "----\n{{")
-        moveSAName = addLineToTop.replace("|SA name:","\n")
-        rEqualSign = moveSAName.replace(" =",":")
-        rR1 = rEqualSign.replace("file:Rainbow icon.png|30px|link=Rainbow Ki","Rainbow")
-        rA1 = rR1.replace("File:AGL ","")
-        rT1 = rA1.replace("File:TEQ ","")
-        rI1 = rT1.replace("File:INT ","")
-        rS1 = rI1.replace("File:STR ","")
-        rP1 = rS1.replace("File:PHY ","")
-        rA2 = rP1.replace("File:EAGL ","")
-        rT2 = rA2.replace("File:ETEQ ","")
-        rI2 = rT2.replace("File:EINT ","")
-        rS2 = rI2.replace("File:ESTR ","")
-        rP2 = rS2.replace("File:EPHY ","")
-        rA3 = rP2.replace("File:SAGL ","")
-        rT3 = rA3.replace("File:STEQ ","")
-        rI3 = rT3.replace("File:SINT ","")
-        rS3 = rI3.replace("File:SSTR ","")
-        rP3 = rS3.replace("File:SPHY ","")
-        rFile = rS3.replace("File:","")
-        rIcon = rFile.replace("icon.png|30px|link=Category:","")
-        rQLB = rIcon.replace('"[[',"")
-        rQRB = rQLB.replace('"]]',"")
-        rLB = rQRB.replace('[[',"")
+        rQuote = addLineToTop.replace('"',"")
+        moveSAName = rQuote.replace("|SA name:","\n")
+        rPettan = moveSAName.replace('|Pettan = yes',"")
+        rTube = rPettan.replace('|','"')
+        rEqualSign = rTube.replace(" =",'":')
+        rLineBreak = rEqualSign.replace('<br>',"").replace('</br>',"")
+
+        rServerIcon = rLineBreak.replace('File:Japan server.png"20px',"").replace('File:Global server.png"20px',"")
+
+        rExtraCatInLink = rServerIcon.replace('Androids-Cell Saga"',"").replace('Kamehameha (Category)"',"").replace('Otherworld Warriors (Link Skill)"',"").replace('Turtle School (Link Skill)"',"").replace('[[Namekians (Link Skill)"',"").replace('Team Bardock (Link Skill)"',"").replace('[[:Category:Extreme Class"',"").replace('[[:Category:Super Class"',"").replace('Fusion (Link Skill)"',"")
+
+        rExtraDisambig = rExtraCatInLink.replace('[[Goku (disambiguation)"',"").replace('[[Goku (disambiguation)#Goku (Youth)"',"").replace('Caulifla (disambiguation)"',"").replace('[[Cooler (disambiguation)"',"").replace('[[Trunks (disambiguation)"',"").replace('[[Bardock (disambiguation)',"").replace('Android 18 (disambiguation)#Android #18"',"").replace('(disambiguation)#Ribrianne"',"").replace('Rozie (disambiguation)"',"").replace('Kakunsa (disambiguation)"',"").replace('Kale (disambiguation)"',"").replace('Tapion (disambiguation)"',"").replace('Gohan (disambiguation)#Gohan (Kid)"',"").replace('Frieza (disambiguation)"',"").('(disambiguation)"Vegeta (Kid, Jr., etc. excluded)',"")
+
+        rSAType = rExtraDisambig.replace('"SA type": Ki',"").replace('"SA type": M',"").replace('"SA type": W',"").replace('"SA Type:"',"").replace('"UltraSA type" "Ki:',"").replace('"UltraSA type": "M',"")
+        rSphereFile = rSAType.replace('File:Rainbow icon.png"30px"link=',"").replace('File:AGL icon.png"30px"link=Category:',"").replace('File:AGL  icon.png"30px"link=Category:',"").replace('File:TEQ icon.png"30px"link=Category:',"").replace('File:INT icon.png"30px"link=Category:',"").replace('File: INT icon.png"30px"link=Category:',"").replace('File:STR icon.png"30px"link=Category:',"").replace('File:PHY icon.png"30px"link=Category:',"")
+        rSSphereFile = rSphereFile.replace('File:SAGL icon.png"30px"link=Category:',"").replace('File:STEQ icon.png"30px"link=Category:',"").replace('File:SINT icon.png"30px"link=Category:',"").replace('File:SSTR icon.png"30px"link=Category:',"").replace('File:SPHY icon.png"30px"link=Category:',"")
+        rESphereFile = rSSphereFile.replace('File:EAGL icon.png"30px"link=Category:',"").replace('File:ETEQ icon.png"30px"link=Category:',"").replace('File:EINT icon.png"30px"link=Category:',"").replace('File:ESTR icon.png"30px"link=Category:',"").replace('File:EPHY icon.png"30px"link=Category:',"")
+        rStackAtt = rESphereFile.replace('([[Stack Attack"How does it work?]])',"").replace('[[Super Attack Multipliers"SA Multiplier]]',"")
+        rName1 = rStackAtt.replace(' name="[1]"',"").replace('name=[1]',"")
+        rName2 = rName1.replace(' name="[2]"',"").replace(' name=[2]',"")
+        rName3 = rName2.replace(' name="[3]"',"").replace(' name=[3]',"")
+        rName4 = rName3.replace(' name="[4]"',"").replace(' name=[4]',"")
+        rName5 = rName4.replace(' name="[5]"',"").replace(' name=[5]',"")
+        rName6 = rName5.replace(' name="[6]"',"").replace(' name=[6]',"")
+        rName7 = rName6.replace(' name="[7]"',"").replace(' name=[7]',"")
+        rName8 = rName7.replace(' name="[8]"',"").replace(' name=[8]',"")
+        rName9 = rName8.replace(' name="[9]"',"").replace(' name=[9]',"")
+        rName10 = rName9.replace(' name=[10]',"").replace(' name=[10]',"")
+
+        rIFrame = rName10.replace('<i>',"").replace('</i>',"")
+        rRef = rIFrame.replace('<ref>',"  <").replace('<ref >',"  <")
+        rRef2 = rRef.replace('</ref>',">  ")
+        rDash = rRef2.replace(' - ',",")
+        rLB = rDash.replace('[[',"")
         rRB = rLB.replace(']]',"")
-        rDash = rRB.replace(' - ',", ")
-        rBr = rDash.replace('<br>'," ")
-        rRef = rBr.replace('<ref>'," ")
-        rRef2 = rRef.replace('</ref>',"")
-        rRef3 = rRef2.replace('ref ',"")
-        rRef4 = rRef3.replace('<>',"+")
-        rQuote = rRef4.replace('"',"")
-        rName1 = rQuote.replace('name="[1]"',"")
-        rName2 = rName1.replace('name="[2]"',"")
-        rName3 = rName2.replace('name="[3]"',"")
-        rName4 = rName3.replace('name="[4]"',"")
-        rName5 = rName4.replace('name="[5]"',"")
-        rName6 = rName5.replace('name="[6]"',"")
-        rName7 = rName6.replace('name="[7]"',"")
-        rName8 = rName7.replace('name="[8]"',"")
-        rName9 = rName8.replace('name="[9]"',"")
-        rName10 = rName9.replace('name=[10]',"")
-        rName11 = rName10.replace('name=[1]',"")
-        rName12 = rName11.replace('name=[2]',"")
-        rName13 = rName12.replace('name=[3]',"")
-        rName14 = rName13.replace('name=[4]',"")
-        rName15 = rName14.replace('name=[5]',"")
-        rName16 = rName15.replace('name=[6]',"")
-        rName17 = rName16.replace('name=[7]',"")
-        rName18 = rName17.replace('name=[8]',"")
-        rName19 = rName18.replace('name=[9]',"")
-        rName20 = rName19.replace('name=[10]',"")
-        rTS = rName20.replace(' (Link Skill)|Turtle School,',"")
-        rSAM = rTS.replace("Super Attack Multipliers|","")
-        rKCat = rSAM.replace('Kamehameha (Category)|', "")
-        rACat = rKCat.replace('|Androids/Cell Saga', "")
-        rSAT = rACat.replace('|SA type: Ki ',"")
 
-
-        test = io.StringIO(rSAT)
+        # ======= this is POST-EDITING print =======
+        print(rRB)
+        test = io.StringIO(rRB)
         myline = test.readline()
 
-        # start attribute grabbing from cards
+        # start attribute grabbing from cards ==================
         extraSpace1 = '{\n'
-        charName1 = "|None \n"
-        charName2 = "|None \n"
-        charRarity = "|None \n"
-        charType = "|None \n"
-        charID = "|None \n"
-        charLS = "|None \n"
-        charSaType = "|None \n"
-        charSaDesc = "|None \n"
-        charUltra = "|None \n"
-        charUltraDesc = "|None \n"
-        charPsName = "|None \n"
-        charPsDesc = "|None \n"
-        charASName = "|None \n"
-        charAS = "|None \n"
-        charASCond = "|None \n"
-        charTransformType = "|None \n"
-        charTransformCond = "|None \n"
-        charLinkSkills = "|None \n"
-        charCategories = "|None \n"
+        charName1 = ""
+        charName2 = ""
+        charRarity = ""
+        charType = ""
+        charID = ""
+        charLS = ""
+        charSaType = ""
+        charSaDesc = ""
+        charUltra = ""
+        charUltraDesc = ""
+        charPsName = ""
+        charPsDesc = ""
+        charASName = ""
+        charAS = ""
+        charASCond = ""
+        charTransformType = ""
+        charTransformCond = ""
+        charLinkSkills = ""
+        finalLinks = ""
+        charCategories = ""
+        finalCategories = ""
         extraSpace2 = '},\n'
         while myline:
-            # print(myline)
             myline = test.readline()
-            if '|name1:' in myline:
-                # print(">>nameone found<<")
-                charName1 = myline
-            if '|name2:' in myline:
-                # print(">>nametwo found<<")
-                charName2 = myline
-            if '|rarity:' in myline:
-                # print(">>rarity found<<")
-                charRarity = myline
-            if '|type:' in myline:
-                # print(">>type found<<")
-                charType = myline
-            if '|ID:' in myline:
-                # print(">>ID found<<")
-                charID = myline
-            if '|LS description:' in myline:
-                # print(">>LS found<<")
-                charLS = myline
-            if '|SA name:' in myline:
-                # print(">>SA type found<<")
-                charSaType = myline
-            if '|SA description:' in myline:
-                # print(">>SA description found<<")
-                charSaDesc = myline
-            if '|UltraSA name:' in myline:
-                # print(">>UltraSA found<<")
-                charUltra = myline
-            if '|UltraSA description:' in myline:
-                # print(">>UltraSa Description found<<")
-                charUltraDesc = myline
-            if '|PS name:' in myline:
-                # print(">>PS name found<<")
-                charPsName = myline
-            if '|PS description:' in myline:
-                # print(">>PS descriotion found<<")
-                charPsDesc = myline
-            if '|Active Skill name:' in myline:
-                # print(">>Active Skill name found<<")
-                charASName = myline
-            if '|Active Skill:' in myline:
-                # print(">>Active Skill found<<")
-                charAS = myline
-            if '|Active Skill condition:' in myline:
-                # print(">>Active Skill condition found<<")
-                charASCond = myline
-            if '|Transform type:' in myline:
-                # print(">>Transform type found<<")
-                charTransformType = myline
-            if '|Transform condition:' in myline:
-                # print(">>Transform description found<<")
-                charTransformCond = myline
-            if '|Link skill:' in myline:
-                # print(">>Link skills found<<")
-                charLinkSkills = myline
-            if '|Category:' in myline:
-                # print(">>Link skills found<<")
-                charCategories = myline
-            
-            results = [extraSpace1, charName1, charName2, charRarity, charType, charID, charLS, charSaType, charSaDesc, charUltra, charUltraDesc, charPsName, charPsDesc, charASName, charAS, charASCond, charTransformType, charTransformCond, charLinkSkills, charCategories, extraSpace2]
-        print(results)
-        print('')
+            if '"name1": ' in myline:
+                charName1 = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"name2":' in myline:
+                charName2 = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"rarity":' in myline:
+                charRarity = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"type":' in myline:
+                charType = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"ID":' in myline:
+                charID = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"LS description":' in myline:
+                charLS = myline .replace(': ',': "',1).replace('\n','",\n')
+            if '"SA name":' in myline:
+                charSaType = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"SA description":' in myline:
+                charSaDesc = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"UltraSA name":' in myline:
+                charUltra = myline.replace(': ',': "',1).replace('\n','",\n')
+            if 'UltraSA description:' in myline:
+                charUltraDesc = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"PS name":' in myline:
+                charPsName = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"PS description":' in myline:
+                charPsDesc = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"Active Skill name":' in myline:
+                charASName = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"Active Skill":' in myline:
+                charAS = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"Active Skill condition":' in myline:
+                charASCond = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"Transform type":' in myline:
+                charTransformType = myline.replace(': ',': "',1).replace('\n','",\n')
+            if '"Transform condition":' in myline:
+                charTransformCond = myline.replace(': ',': ["',1).replace('\n','"],\n')
+            if '"Link skill":' in myline:
+                charLinkSkills = myline.replace(",",'","').replace('" ','"').replace(': ',': ["').replace('\n','"],\n')
+            if '"Category":' in myline:
+                charCategories = myline.replace(",",'","').replace('" ','"').replace(': ',': ["',1).replace('\n','"]\n')
 
-        with open('out.txt', 'a', encoding='utf-8') as output:
+            if charLS == "":
+                results =[]
+            if charCategories == "":
+                results =[]
+            if "SSR" in charRarity:
+                results =[]
+            else:
+                results = [extraSpace1, charName1, charName2, charRarity, charType, charID, charLS, charSaType, charSaDesc, charUltra, charUltraDesc, charPsName, charPsDesc, charASName, charAS, charASCond, charTransformType, charTransformCond, charLinkSkills, charCategories, extraSpace2]
+        
+        with open('out.json', 'a', encoding='utf-8') as output:
             output.writelines(results)
